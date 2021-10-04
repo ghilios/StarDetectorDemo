35f5ef37688225ce7e301010bdbd1e45b9ec79ea
George Hilios <ghilios@gmail.com>
2021-10-04 12:37:34 -0400

    First commit

---
+++ StarDetectorDemo/ImageUtility.cs
@@ -0,0 +1,246 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarDetectorDemo {
+    public static class ImageUtility {
+        public class Statistics {
+            public double Median { get; set; }
+            public double MAD { get; set; }
+        }
+
+        private static double NormalizeUShort(double val, int bitDepth) {
+            return val / (double)(1 << bitDepth);
+        }
+
+        private static ushort DenormalizeUShort(double val) {
+            return (ushort)(val * ushort.MaxValue);
+        }
+
+        private static double MidtonesTransferFunction(double midToneBalance, double x) {
+            if (x > 0) {
+                if (x < 1) {
+                    return (midToneBalance - 1) * x / ((2 * midToneBalance - 1) * x - midToneBalance);
+                }
+                return 1;
+            }
+            return 0;
+        }
+
+        public static void ApplyLUT(Mat src, Mat lut, Mat dst) {
+            if (dst.Size() != src.Size() || dst.Type() != src.Type()) {
+                dst.Create(src.Size(), src.Type());
+            }
+
+            unsafe {
+                var srcData = (ushort*)src.DataPointer;
+                var dstData = (ushort*)dst.DataPointer;
+                var lutData = (ushort*)lut.DataPointer;
+                var numPixels = src.Rows * src.Cols;
+                for (int i = 0; i < numPixels; ++i) {
+                    dstData[i] = lutData[srcData[i]];
+                }
+            }
+        }
+
+        public static Mat CreateMTFLookup(Statistics statistics, double shadowsClipping = -2.8, double targetHistogramMedianPercent = 0.2) {
+            var normalizedMedian = NormalizeUShort(statistics.Median, 16);
+            var normalizedMAD = NormalizeUShort(statistics.MAD, 16);
+
+            var scaleFactor = 1.4826; // see https://en.wikipedia.org/wiki/Median_absolute_deviation
+
+            double shadows = normalizedMedian + shadowsClipping * normalizedMAD * scaleFactor;
+            double midtones = MidtonesTransferFunction(targetHistogramMedianPercent, normalizedMedian - shadows);
+            double highlights = 1;
+
+            var lut = new Mat(1, ushort.MaxValue, MatType.CV_16U);
+            unsafe {
+                var lutData = (ushort*)lut.DataPointer;
+                for (int i = 0; i < ushort.MaxValue; i++) {
+                    double value = NormalizeUShort(i, 16);
+                    lutData[i] = DenormalizeUShort(MidtonesTransferFunction(midtones, 1 - highlights + value - shadows));
+                }
+            }
+
+            return lut;
+        }
+
+        private static uint[] CalculateHistogram(Mat image) {
+            var histogram = new uint[ushort.MaxValue + 1];
+            unsafe {
+                var dataPtr = (ushort*)image.DataPointer;
+                for (var i = 0; i < image.Rows * image.Cols; ++i) {
+                    histogram[dataPtr[i]] += 1;
+                }
+            }
+            return histogram;
+        }
+
+        public static Statistics CalculateStatistics(Mat image) {
+            var histogram = CalculateHistogram(image);
+            var targetMedianCount = image.Rows * image.Cols / 2.0;
+            uint currentCount = 0;
+            double median = -1.0d;
+            for (uint i = 0; i <= ushort.MaxValue; ++i) {
+                currentCount += histogram[i];
+                if (currentCount > targetMedianCount) {
+                    median = i;
+                    break;
+                } else if (currentCount == targetMedianCount) {
+                    for (uint j = i + 1; i <= ushort.MaxValue; ++i) {
+                        if (histogram[j] > 0) {
+                            median = (i + j) / 2.0d;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+
+            currentCount = 0;
+            int upIndex = (int)Math.Ceiling(median);
+            int downIndex = (int)Math.Floor(median);
+            double beforeMedian = -1;
+            double mad;
+            while (true) {
+                while (upIndex <= ushort.MaxValue && histogram[upIndex] == 0) {
+                    ++upIndex;
+                }
+                while (downIndex >= 0 && histogram[downIndex] == 0) {
+                    --downIndex;
+                }
+
+                var upDistance = upIndex <= ushort.MaxValue ? Math.Abs(upIndex - median) : double.MaxValue;
+                var downDistance = downIndex >= 0 ? Math.Abs(downIndex - median) : double.MaxValue;
+                int chosenIndex;
+                if (upDistance <= downDistance) {
+                    chosenIndex = upIndex;
+                    currentCount += histogram[upIndex++];
+                } else {
+                    chosenIndex = downIndex;
+                    currentCount += histogram[downIndex--];
+                }
+
+                if (currentCount == targetMedianCount) {
+                    beforeMedian = Math.Abs(chosenIndex - median);
+                } else if (currentCount > targetMedianCount) {
+                    if (beforeMedian >= 0) {
+                        mad = (beforeMedian + Math.Abs(chosenIndex - median)) / 2.0d;
+                    } else {
+                        mad = Math.Abs(chosenIndex - median);
+                    }
+                    break;
+                }
+            }
+
+            return new Statistics() { Median = median, MAD = mad };
+        }
+
+        private static Mat SeparatedB3SplineScalingFilter(int dyadicLayer) {
+            /*
+             * Cubic Spline coefficients
+             *   1.0/256, 1.0/64, 3.0/128, 1.0/64, 1.0/256,
+             *   1.0/64,  1.0/16, 3.0/32,  1.0/16, 1.0/64,
+             *   3.0/128, 3.0/32, 9.0/64,  3.0/32, 3.0/128,
+             *   1.0/64,  1.0/16, 3.0/32,  1.0/16, 1.0/64,
+             *   1.0/256, 1.0/64, 3.0/128, 1.0/64, 1.0/256
+             *   
+             * Decomposed 1D filter
+             *   0.0625,  0.25,   0.375,   0.25,   0.0625
+             */
+
+            int size = (1 << (dyadicLayer + 2)) + 1;
+            var bicubicWavelet2D = new Mat(new Size(1, size), MatType.CV_32F, 0.0d);
+            // Each successive layer is downsampled 2x. Rather than copy the matrix to convolve it, we can padd
+            // the separated filter with zeroes
+            unsafe {
+                var data = (float*)bicubicWavelet2D.DataPointer;
+                data[0] = data[size - 1] = 0.0625f;
+                data[1 << dyadicLayer] = data[size - (1 << dyadicLayer) - 1] = 0.25f;
+                data[size >> 1] = 0.375f;
+            }
+            return bicubicWavelet2D;
+        }
+
+        public static Mat Clip(Mat src, float min = 0.0f, float max = 1.0f) {
+            unsafe {
+                var srcData = (float*)src.DataPointer;
+                var numPixels = src.Rows * src.Cols;
+                for (int i = 0; i < numPixels; ++i) {
+                    srcData[i] = Math.Min(1.0f, Math.Max(0.0f, srcData[i]));
+                }
+                return src;
+            }
+        }
+
+        public static Mat[] ComputeAtrousB3SplineDyadicWaveletLayers(Mat src, int numLayers) {
+            var layers = new Mat[numLayers + 1];
+            var previousLayer = src;
+            Mat convolved = new Mat();
+            for (int i = 0; i < numLayers; ++i) {
+                var scalingFilter = SeparatedB3SplineScalingFilter(i);
+                Cv2.SepFilter2D(previousLayer, convolved, MatType.CV_32F, scalingFilter, scalingFilter, borderType: BorderTypes.Reflect);
+                var nextLayer = previousLayer.Subtract(convolved).ToMat();
+                previousLayer = layers[i] = Clip(nextLayer);
+            }
+            layers[numLayers] = convolved;
+            return layers;
+        }
+
+        public static double KappaSigmaNoiseEstimate(Mat src, out long numBackgroundPixels, double clippingMultipler = 3.0d, double allowedError = 0.05, int maxIterations = 10) {
+            // NOTE: This algorithm could be sped up for 16-byte integer data by building a histogram. Consider this if performance becomes problematic
+            // TODO: Assert input data are float
+
+            // Gaussian noise scaling factors:
+            //  { 0.8907F, 0.2007F, 0.0856F, 0.0413F, 0.0205F, 0.0103F, 0.0052F, 0.0026F, 0.0013F, 0.0007F };
+            unsafe {
+                var srcData = (float*)src.DataPointer;
+                int numPixels = src.Rows * src.Cols;
+                var threshold = float.MaxValue;
+                var lastSigma = 1.0d;
+                var backgroundPixels = 0L;
+                int numIterations = 0;
+                while (numIterations < maxIterations) {
+                    ++numIterations;
+                    var total = 0.0d;
+                    backgroundPixels = 0L;
+                    for (int i = 0; i < numPixels; ++i) {
+                        var pixel = srcData[i];
+                        if (pixel < threshold && pixel > 0.0f) {
+                            total += pixel;
+                            ++backgroundPixels;
+                        }
+                    }
+
+                    var mean = total / backgroundPixels;
+                    var mrs = 0.0d;
+                    for (int i = 0; i < numPixels; ++i) {
+                        var pixel = srcData[i];
+                        if (pixel < threshold) {
+                            var error = pixel - mean;
+                            mrs += error * error;
+                        }
+                    }
+
+                    var variance = mrs / (backgroundPixels - 1);
+                    var sigma = Math.Sqrt(variance);
+                    if (numIterations > 1) {
+                        var sigmaConvergenceError = Math.Abs(sigma - lastSigma) / lastSigma;
+                        if (sigmaConvergenceError <= allowedError) {
+                            lastSigma = sigma;
+                            break;
+                        }
+                    }
+                    threshold = (float)(mean + clippingMultipler * sigma);
+                    lastSigma = sigma;
+                }
+
+                numBackgroundPixels = backgroundPixels;
+                return lastSigma;
+            }
+        }
+    }
+}
---
+++ StarDetectorDemo/Program.cs
@@ -0,0 +1,248 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp.Internal.Util;
+using OpenCvSharp.Internal.Vectors;
+using System.Buffers;
+
+namespace StarDetectorDemo {
+    class Program {
+        static async Task Main(string[] args) {
+            var path = @"C:\AP\LIGHT_2021-07-29_02-36-57_H_-9.90_300.00s_0151.tif";
+            bool hotpixelFiltering = false;
+            int noiseReductionRadius = 1;
+            using (var t = new ResourcesTracker()) {
+                var src = t.T(new Mat(path, ImreadModes.Unchanged));
+                var statistics = ImageUtility.CalculateStatistics(src);
+                var lut = t.T(ImageUtility.CreateMTFLookup(statistics));
+
+                var starDetectionSrc = t.NewMat();
+                ConvertToFloat(src, starDetectionSrc);
+                if (hotpixelFiltering || noiseReductionRadius > 0) {
+                    // Filter hotpixels before noise reduction, otherwise hotpixels may be promoted to stars
+                    HotpixelFilter(starDetectionSrc, starDetectionSrc);
+                }
+                if (noiseReductionRadius > 0) {
+                    var gaussianKernel = Cv2.GetGaussianKernel(1 + (1 << noiseReductionRadius), sigma: 2.0d);
+                    Cv2.SepFilter2D(starDetectionSrc, starDetectionSrc, starDetectionSrc.Type(), gaussianKernel, gaussianKernel);
+                }
+
+                Mat structureMapSrc = starDetectionSrc;
+                if (!hotpixelFiltering && noiseReductionRadius == 0) {
+                    structureMapSrc = t.NewMat();
+                    HotpixelFilter(starDetectionSrc, structureMapSrc);
+                }
+
+                var structureMap = t.NewMat();
+                ComputeStructureMap(structureMapSrc, structureMap, structureLayers: 5);
+
+                starDetectionSrc.SaveImage(@"C:\AP\star-detection-src.tif");
+                structureMap.SaveImage(@"C:\AP\structure-map.tif");
+                /*
+                processed.SaveImage(@"C:\AP\processed.tif");
+                processed.ConvertTo(processed, MatType.CV_16U);
+                var processedStatistics = ImageUtility.CalculateStatistics(processed);
+                var processedLut = t.T(ImageUtility.CreateMTFLookup(processedStatistics));
+
+                var stretchedSrc = t.NewMat();
+                ImageUtility.ApplyLUT(src, lut, stretchedSrc);
+                ImageUtility.ApplyLUT(processed, processedLut, processed);
+
+                var srcWindow = t.T(new Window("Original Image", stretchedSrc, WindowFlags.Normal | WindowFlags.KeepRatio));
+                var processedWindow = t.T(new Window("Processed Image", processed, WindowFlags.Normal | WindowFlags.KeepRatio));
+                Cv2.WaitKey();
+                var srcVisible = Cv2.GetWindowProperty(srcWindow.Name, WindowPropertyFlags.Visible);
+                if (srcVisible == 0) {
+                    srcWindow.IsEnabledDispose = false;
+                }
+
+                var provessedVisible = Cv2.GetWindowProperty(processedWindow.Name, WindowPropertyFlags.Visible);
+                if (provessedVisible == 0) {
+                    processedWindow.IsEnabledDispose = false;
+                }
+                */
+
+                Console.WriteLine();
+            }
+        }
+
+        static void HotpixelFilter(Mat src, Mat dst) {
+            // Hotpixel filtering with radius=1 uses a median morphological filter with a 3x3 box
+            // If we want to support larger radii we'd have to implement a custom FilterEngine that takes a circular binary mask
+            Cv2.MedianBlur(src, dst, 3);
+        }
+
+        static void ComputeStructureMap(Mat src, Mat dst, int structureLayers = 5) {
+            if (src.Size() != dst.Size() || src.Type() != dst.Type()) {
+                dst.Create(src.Size(), src.Type());
+            }
+
+            using (var tempImage = new Mat())
+            using (var boxFilter = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(3, 3))) {
+                // Step 1: Flatten the image with a high-pass filter
+                var gaussianKernel = Cv2.GetGaussianKernel(1 + (1 << structureLayers), sigma: 5.3d);
+
+                src.SaveImage(@"C:\AP\structure-src.tif");
+
+                Cv2.SepFilter2D(src, tempImage, src.Type(), gaussianKernel, gaussianKernel);
+                tempImage.SaveImage(@"C:\AP\structure-blurred.tif");
+                using (var t = src.Subtract(tempImage).ToMat()) {
+                    ImageUtility.Clip(t);
+                    unsafe {
+                        var imageData = (float*)t.DataPointer;
+                        var numPixels = t.Rows * t.Cols;
+                        float min = float.MaxValue;
+                        float max = -1.0f;
+                        for (int i = 0; i < numPixels; ++i) {
+                            var pixel = imageData[i];
+                            min = pixel < min ? pixel : min;
+                            max = pixel > max ? pixel : max;
+                        }
+                        Console.WriteLine($"MAX: {max}, MIN: {min}");
+                        using (var u = t.Subtract(min).Divide(max - min).ToMat()) {
+                            u.CopyTo(tempImage);
+                        }
+                    }
+                }
+
+                tempImage.SaveImage(@"C:\AP\structure-step-1.tif");
+
+                // Step 2: Boost small structures with a dilation box filter
+                Cv2.MorphologyEx(tempImage, tempImage, MorphTypes.Dilate, boxFilter);
+
+                tempImage.SaveImage(@"C:\AP\structure-step-2.tif");
+
+                // Step 3: Binarize foreground structures based on noise estimates
+                // TODO: Dispose wavelet layers afterwards
+                var noiseClippingMultiplier = 3.0d;
+
+                // The 2nd wavelet layer is a cleaner image more likely to contain stars and less likely to include noise
+                var waveletLayers = ImageUtility.ComputeAtrousB3SplineDyadicWaveletLayers(tempImage, 2);
+                long numBackgroundPixels;
+                var ksigmaNoise = ImageUtility.KappaSigmaNoiseEstimate(waveletLayers[1], out numBackgroundPixels, clippingMultipler: noiseClippingMultiplier) / 0.2007f;
+                var backgroundPercentage = (double)numBackgroundPixels / (tempImage.Rows * tempImage.Cols);
+                Console.WriteLine($"KSigma Noise Estimate: {ksigmaNoise}. Background Pixels = {backgroundPercentage}");
+
+                // TODO: This median calculation is wrong, since tempImage is float
+                double dilatedMedian;
+                using (var medianMat = new Mat()) {
+                    float[] data;
+                    tempImage.GetArray<float>(out data);
+
+                    Array.Sort(data);
+                    // ConvertToUShort(tempImage, medianMat);
+                    // var med = ImageUtility.CalculateStatistics(medianMat).Median;
+                    var med = data[data.Length >> 1];
+                    Console.WriteLine($"Median is {med}");
+                    // dilatedMedian = med / ushort.MaxValue;
+                    dilatedMedian = med;
+                }
+                Console.WriteLine($"Median: {dilatedMedian}, Threshold = {dilatedMedian + noiseClippingMultiplier * ksigmaNoise}");
+                Binarize(tempImage, tempImage, (float)(dilatedMedian + noiseClippingMultiplier * ksigmaNoise));
+
+                tempImage.SaveImage(@"C:\AP\structure-step-3.tif");
+
+                tempImage.CopyTo(dst);
+            }
+        }
+
+        static void ConvertToFloat(Mat src, Mat dst) {
+            if (src.Size() != dst.Size() || dst.Type() != MatType.CV_32F) {
+                dst.Create(src.Size(), MatType.CV_32F);
+            }
+            unsafe {
+                var srcData = (ushort*)src.DataPointer;
+                var dstData = (float*)dst.DataPointer;
+                var numPixels = src.Rows * src.Cols;
+                var maxShort = (float)ushort.MaxValue;
+                for (int i = 0; i < numPixels; ++i) {
+                    dstData[i] = (float)srcData[i] / maxShort;
+                }
+            }
+        }
+
+        static void Binarize(Mat src, Mat dst, float threshold) {
+            if (src.Size() != dst.Size() || src.Type() != dst.Type()) {
+                dst.Create(src.Size(), src.Type());
+            }
+            unsafe {
+                var srcData = (float*)src.DataPointer;
+                var dstData = (float*)dst.DataPointer;
+                var numPixels = src.Rows * src.Cols;
+                for (int i = 0; i < numPixels; ++i) {
+                    dstData[i] = srcData[i] >= threshold ? 1.0f : 0.0f;
+                }
+            }
+        }
+
+        static void ConvertToUShort(Mat src, Mat dst) {
+            // TODO: Throw if src == dst reference
+            if (src.Size() != dst.Size() || dst.Type() != MatType.CV_16U) {
+                dst.Create(src.Size(), MatType.CV_16U);
+            }
+            unsafe {
+                var srcData = (float*)src.DataPointer;
+                var dstData = (ushort*)dst.DataPointer;
+                var numPixels = src.Rows * src.Cols;
+                for (int i = 0; i < numPixels; ++i) {
+                    dstData[i] = (ushort)(Math.Ceiling(srcData[i] * ushort.MaxValue));
+                }
+            }
+        }
+
+        static void GetBicubicWaveletFilter(Mat src, Mat dst, int layer) {
+            var bicubicWavelet2D = new Mat(new Size(1, 5), MatType.CV_32F);
+            unsafe {
+                var data = (float*)bicubicWavelet2D.DataPointer;
+                data[0] = data[4] = 0.0625f;
+                data[1] = data[3] = 0.25f;
+                data[2] = 0.375f;
+            }
+
+            src.SaveImage(@"C:\AP\src.tif");
+
+            var srcToFloat = new Mat();
+            ConvertToFloat(src, srcToFloat);
+
+            Cv2.ImWrite(@"C:\AP\src_to_float.tif", srcToFloat);
+            //csrcToFloat.SaveImage();
+
+            var convolved = new Mat();
+            Cv2.SepFilter2D(srcToFloat, convolved, MatType.CV_32F, bicubicWavelet2D, bicubicWavelet2D, borderType: BorderTypes.Reflect);
+
+            convolved.SaveImage(@"C:\AP\convolved.tif");
+
+            var result = srcToFloat.Subtract(convolved).ToMat();
+            /*
+            var result = new Mat(srcToFloat.Size(), MatType.CV_32F);
+            unsafe {
+                var srcData = (float*)srcToFloat.DataPointer;
+                var convolvedData = (float*)convolved.DataPointer;
+                var resultData = (float*)result.DataPointer;
+                var numPixels = result.Rows * result.Cols;
+                for (int i = 0; i < numPixels; ++i) {
+                    if (srcData[i] <= convolvedData[i]) {
+                        resultData[i] = srcData[i];
+                    } else {
+                        resultData[i] = 0.0f;
+                    }
+                }
+            }
+            */
+
+            unsafe {
+                var resultData = (float*)result.DataPointer;
+                var numPixels = result.Rows * result.Cols;
+                for (int i = 0; i < numPixels; ++i) {
+                    resultData[i] = Math.Min(1.0f, Math.Max(0.0f, resultData[i]));
+                }
+            }
+
+            result.SaveImage(@"C:\AP\subtracted.tif");
+            result.CopyTo(dst); // .ConvertTo(dst, MatType.CV_16U);
+        }
+    }
+}
